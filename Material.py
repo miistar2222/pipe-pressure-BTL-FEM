1462b5360ae69cb01af8c9222997e12ee66643c8
miistar2222 <minhphamduc51205@gmail.com>
2026-04-12 23:19:41 +0700

    chú thích mesh

--- Material.py
+++ Material.py
@@ -8,7 +8,7 @@ class Material:
 
     def compute_D(self): 
         E, nu = self.E, self.nu
-        coef = E / ((1 + nu) * (1 - 2 * nu))
+        coef = E / ((1 + nu) * (1 - 2 * nu)) ### không được để v=0.5
         #ma trận biến dạng phẳng
         return coef * np.array([
             [1 - nu, nu    , 0],
